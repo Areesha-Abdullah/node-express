d8dc258bbb7cfa129eb987961954e04b211c8fd0
Areesha-Abdullah <areesha.abdul23@gmail.com>
2024-08-02 16:40:00 +0500

    up

--- 03-task-manager/final/controllers/tasks.js
+++ 03-task-manager/final/controllers/tasks.js
@@ -12,8 +12,8 @@ const createTask = asyncWrapper(async (req, res) => {
 })
 
 const getTask = asyncWrapper(async (req, res, next) => {
-  const { id: taskID } = req.params.id;
-  const task = await Task.findOne({ where: { id: taskID } });  
+  const { id: taskID } = req.params
+  const task = await Task.findOne({ _id: taskID })
   if (!task) {
     return next(createCustomError(`No task with id : ${taskID}`, 404))
   }
